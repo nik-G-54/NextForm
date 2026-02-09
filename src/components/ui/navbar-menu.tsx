480349fc1b4e26ade247cbeadc2b0ee7664caf94
Nikhil <nikhilgupta542006@gmail.com>
2026-02-09 21:57:52 +0530

    small changes are done

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -37,6 +37,12 @@ export const MenuItem = ({
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={transition}
         >
+      {/* {active !== null && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.85, y: 10 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          transition={transition}
+        > */}
           {active === item && children && (
             <div className="absolute top-[calc(100%_+_1.2rem)] left-1/2 transform -translate-x-1/2 pt-4">
               <motion.div
@@ -104,6 +110,54 @@ export const ProductItem = ({
     </a>
   );
 };
+// export const Menu = ({
+//   setActive,
+//   children,
+// }: {
+//   setActive: (item: string | null) => void;
+//   children: React.ReactNode;
+// }) => {
+//   return (
+//     <nav
+//       onMouseLeave={() => setActive(null)}
+//       className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6"
+//     >
+//       {children}
+//     </nav>
+//   );
+// };
+
+// export const ProductItem = ({
+//   title,
+//   description,
+//   href,
+//   src,
+// }: {
+//   title: string;
+//   description: string;
+//   href: string;
+//   src: string;
+// }) => {
+//   return (
+//     <a href={href} className="flex space-x-2">
+//       <img
+//         src={src}
+//         width={140}
+//         height={70}
+//         alt={title}
+//         className="shrink-0 rounded-md shadow-2xl"
+//       />
+//       <div>
+//         <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+//           {title}
+//         </h4>
+//         <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
+//           {description}
+//         </p>
+//       </div>
+//     </a>
+//   );
+// };
 
 /* ---------- FIXED PART ---------- */
 
